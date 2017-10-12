acdda0a6650ed915dacf057469ab262964811c91
Christopher Gallup <chris.gallup@hotmail.com>
2017-10-11 17:46:23 -1200

    Add active class to current nav li

--- site/js/app.js
+++ site/js/app.js
@@ -1,1 +1,20 @@
-console.log('works'); 
+var counter = 0; 
+$(function(){
+    var current_page_URL = location.href;
+    $( "a" ).each(function() {
+        if ($(this).attr("href") !== "#") {
+            counter++; 
+            var target_URL = $(this).prop("href");
+            if (target_URL == current_page_URL) {
+            $('nav a').parents('li, ul').removeClass('active');
+            $(this).parent('li').addClass('active');
+                return false;
+            } 
+        }
+    }); 
+    if(counter === 1) {
+        $('#home').addClass('active'); 
+    } else {
+        $('#home').removeClass('active'); 
+    }
+});
